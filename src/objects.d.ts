44f4a81dcb4253cacf3bff98893e8ff614323274
hywax <a.hywax@gmail.com>
2023-11-29 20:09:17 +0500

    feat(objects): types for `keys` and `entries`

---
+++ src/objects.d.ts
@@ -0,0 +1,10 @@
+interface ObjectConstructor {
+  keys<O extends any[]>(obj: O): Array<keyof O>
+  keys<O extends Record<Readonly<string>, any>>(obj: O): Array<keyof O>
+  keys(obj: object): string[]
+
+  entries<T extends { [K: Readonly<string>]: any }>(obj: T): Array<[keyof T, T[keyof T]]>
+  entries<T extends object>(obj: { [s: string]: T } | ArrayLike<T>): [string, T[keyof T]][]
+  entries<T>(obj: { [s: string]: T } | ArrayLike<T>): [string, T][]
+  entries(obj: object): [string, any][]
+}
